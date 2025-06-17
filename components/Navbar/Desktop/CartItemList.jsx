4c00e7a6ecbf9bf04bfbbf77b9d501d8270d3b0e
TomasMazz <tomasmazzucco11@gmail.com>
2025-06-17 20:06:30 +0200

    Mejora al agregar el producto al carrtio + imagen + detalles

--- components/Navbar/Desktop/CartItemList.jsx
+++ components/Navbar/Desktop/CartItemList.jsx
@@ -1,19 +1,43 @@
-// components/CartItemList.jsx
 export default function CartItemList({ items }) {
-  if (items.length === 0)
+  if (items.length === 0) {
     return (
-      <div className="py-6 text-gray-500 text-center">El carrito está vacío.</div>
+      <div className="py-6 text-gray-500 text-center">
+        El carrito está vacío.
+      </div>
     );
+  }
 
   return (
-    <ul className="flex flex-col gap-4 text-black">
+    <ul className="flex flex-col gap-4">
       {items.map((item, i) => (
-        <li key={i} className="flex justify-between items-center text-sm">
-          <span className="font-medium">
-            {item.name}{" "}
-            <span className="text-gray-500 text-sm">x{item.quantity}</span>
-          </span>
-          <span>${item.price * item.quantity}</span>
+        <li key={i}>
+          <div className="p-4 flex items-center justify-between">
+
+            {/* Imagen a la izquierda */}
+            <img
+              src={item.imageSrc}
+              alt={item.name}
+              className="w-16 h-16 object-cover rounded"
+            />
+
+            {/* Título + “Envío no incluido” en el centro */}
+            <div className="flex-1 mx-4 text-black">
+              <div className="font-semibold text-sm">
+                {item.name}
+              </div>
+              <div className="text-xs mt-1 mr-1 inline-block px-1 bg-gray-300 border rounded-sm">
+                Envío no incluido
+              </div>
+               <div className="text-xs mt-1 inline-block px-1 bg-gray-300 border rounded-sm">
+                Sin bombilla
+              </div>
+            </div>
+
+            {/* Precio a la derecha */}
+            <div className="font-semibold text-black text-base">
+              ${item.price}
+            </div>
+          </div>
         </li>
       ))}
     </ul>

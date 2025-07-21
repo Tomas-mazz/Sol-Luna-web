5da66865f21ec65e9e7e976edf7aa22f63e995cb
TomasMazz <tomasmazzucco11@gmail.com>
2025-07-21 12:18:50 +0200

    modificacion de backside minimo

--- components/Main/UI/ProductCard/Back/ProductInfo.jsx
+++ components/Main/UI/ProductCard/Back/ProductInfo.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function ProductInfo({ name, description }) {
   return (
-    <div className="text-center space-y-6">
-      <h2 className="text-xl text-black font-semibold">{name}</h2>
-      <p className="text-sm text-black">{description}</p>
+    <div className="text-center space-y-6 text-black">
+      <h2 className="text-xl font-semibold">{name}</h2>
+      <p className="text-sm">{description}</p>
     </div>
   );
 }
--- data/products.js
+++ data/products.js
@@ -3,7 +3,7 @@ export const mateProducts = [
     id: 1,
     name: 'Mate Camionero',
     price: 4000,
-    description: 'Mate tradicional en calabaza natural.',
+    description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
     imageFrontSide: '/imagenes/image-mate-test.jpg',
     imagesBackSide: ['/imagenes/matebackside-1.jpeg', '/imagenes/matebackside-2.jpeg', '/imagenes/matebackside-3.jpeg']
   },
@@ -11,7 +11,7 @@ export const mateProducts = [
     id: 2,
     name: 'Mate Moderno',
     price: 2250,
-    description: 'Mate de acero inoxidable con diseño minimalista.',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
     imageFrontSide: '/imagenes/image-mate-test.jpg',
     imagesBackSide: ['/imagenes/matebackside-1.jpeg', '/imagenes/matebackside-2.jpeg', '/imagenes/matebackside-3.jpeg']
   },
@@ -19,7 +19,7 @@ export const mateProducts = [
     id: 3,
     name: 'Mate de Cerámica',
     price: 2500,
-    description: 'Mate pintado a mano, edición limitada.',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
     imageFrontSide: '/imagenes/image-mate-test.jpg',
     imagesBackSide: ['/imagenes/matebackside-1.jpeg', '/imagenes/matebackside-2.jpeg', '/imagenes/matebackside-3.jpeg']
   },
